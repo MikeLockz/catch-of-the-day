e125e606b2449a95e3460f8d201b4a4f63e9f91f
Mike Lockwitz <mlockwitz@gmail.com>
2019-01-22 16:25:18 -0700

    Adding navigation to store picker component.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this)
+    console.log("Going to create component")
+  }
+  
+  storeNameInput = React.createRef()
+
+  // Method is declared as a property on the component set to an arrow function
+  goToStore = (event) => {
+    event.preventDefault()
+    
+    const storeName = this.storeNameInput.current.value
+
+    this.props.history.push(`/store/${storeName}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2> Please Enter a Store</h2>
-        <input type="text" required placeholder="Enter store name" />
+        <input type="text" 
+          required 
+          placeholder="Enter store name" 
+          defaultValue={getFunName()} 
+          ref={this.storeNameInput}
+        />
 
         <button type="submit">Visit Store</button>
       </form>
